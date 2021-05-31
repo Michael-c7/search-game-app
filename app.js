49122d60fb2dbd35ed3782c631e006b4ee07d6dd
Michael Carr <mcarr2468@gmail.com>
2021-05-31 18:56:27 -0500

    V1.0

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+/*
+<li class="card">
+                    <header class="card__header">
+                            <img class="card__header__img" src="./images/cake-test-image.jpg" alt="cover image">
+                            <div class="card__info">
+                                <h2 class="card__game-title">bioshock 2</h2>
+                                <div class="card__score">65 / 100</div>
+                            </div>
+                    </header>
+                    <div class="card__learn-more">
+                        <button class="card__learn-more-btn">Learn More</button>
+                    </div>
+                </li>
+*/
