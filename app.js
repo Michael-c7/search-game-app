15f780b84569ca95acd9f9d8143fdd4fdc2f75b3
Michael Carr <mcarr2468@gmail.com>
2021-06-02 14:52:38 -0500

    V1.4

--- app.js
+++ app.js
@@ -63,11 +63,6 @@ let renderCards = games => {
     });
 }
 
-// render to DOM
-// getListOfGames().then(data => renderCards(data))
-
-
-
 let getCurrentCardId = _ => {
     if(event.target.closest(".card__learn-more-btn")) {
         return Number(event.target.parentElement.parentElement.id);
@@ -89,7 +84,7 @@ let closePopup = _ => {
 }
 
 
-popupCloseBtn.addEventListener("click", closePopup)
+
 
 
 /*
@@ -105,40 +100,114 @@ cardList.addEventListener("click", event => {
 
 let getAdditionalInfo = gameId => {
     // search for a specific game
-    gameId = 3498;
+    gameId = 19476;
 
     let getInfo = _ => {
-        fetch(`https://api.rawg.io/api/games/${gameId}?key=${apiKey}`)
+        return fetch(`https://api.rawg.io/api/games/${gameId}?key=${apiKey}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            let getGenre = data.genres.map(item => item.name);
+            let getDev = data.developers.map(item => item.name);
+            let getPlatform = data.platforms.map(item => item.platform.name);
+
+            return {
+                name:data.name,
+                releaseDate:data.released,
+                esrbRating:data.esrb_rating.name,
+                genres:getGenre,
+                platforms:getPlatform,
+                developers:getDev,
+                description:data.description_raw,
+            }
         })
     }
-    getInfo()
-    
-
-
-
-
     // get game trailer
-    // https://api.rawg.io/api/games/{id}/movies
-
     let getGameTrailerInfo = _ => {
         return fetch(`https://api.rawg.io/api/games/${gameId}/movies?key=${apiKey}`)
         .then(res => res.json())
         .then(data => {
+
+            /*
+            if data is undefined return a default video
+            */
+
             return {
                 trailer:data.results[0].data.max,
-                trailerName:data.results[0].name,
-                trailerThumbnail:data.results[0].preview
+                name:data.results[0].name,
+                thumbnail:data.results[0].preview,
                 }
         });
     }
 
-    // getGameTrailerInfo().then(item => console.log(item))
+    return {
+        gameInfo:getInfo(),
+        gameTrailerInfo: getGameTrailerInfo(),
+    }
 }
 
 
-// getAdditionalInfo()
+let renderPopupInfo = popupInfo => {
+    let markup = `
+        <div class="popup__info">
+            <h2 class="popup__info__heading">Grand Theft Auto V</h2>
+            <video class="popup__info__video" src="./videos/no-video-available.mp4" controls></video>
+            <div class="popup__info__data">
+                <div class="popup__info__release-date">Release Date: 2021</div>
+                <div class="popup__info__rating">Rating: Mature</div>
+            </div>
 
+            <div class="popup__info__genres">Action, adventure, RPG</div>
+            <div class="popup__info__platforms">Platforms: ps4, ps4, pc, xbox 360</div>
+            <div class="popup__info__developers">Developers: Rockstar North, Rockstar South</div>
+            <p class="popup__info__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
+                alias eos amet error assumenda nobis ipsum numquam dolor quibusdam.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
+            </p>
+        </div>
+        <div class="popup__close">
+                    <button class="popup__close-btn">
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </button>
+                </div>
+    `;
+
+
+    let info = popupInfo.gameInfo.then(item => {
+        const {description, developers, esrbRating,
+        genres, name, platforms, releaseDate} = item;
+        // name
+        document.querySelector(".popup__info__heading").innerHTML = name;
+        // release date
+        document.querySelector(".popup__info__release-date").innerHTML = `Release Date: ${releaseDate.slice(0,4)}`;
+        // rating(esrb)
+        document.querySelector(".popup__info__rating").innerHTML = `Rating: ${esrbRating}`;
+        // genres
+        document.querySelector(".popup__info__genres").innerHTML = `Genres: ${genres}`;
+        // developers
+        document.querySelector(".popup__info__developers").innerHTML = `Developed by: ${developers}`;
+        // platforms
+        document.querySelector(".popup__info__platforms").innerHTML = `Platforms: ${platforms}`;
+        // description
+        document.querySelector(".popup__info__description").innerHTML = description;
+    });
+
+
+    let trailerInfo = popupInfo.gameTrailerInfo.then(item => {
+        const {trailer} = item;
+        document.querySelector(".popup__info__video").src = trailer;
+    })
+    popup.innerHTML = markup;
+}
+
+
+
+let render = () => {
+    getListOfGames().then(data => renderCards(data))
+    renderPopupInfo(getAdditionalInfo(865))
+}
+
+// render()
+
+document.querySelector(".popup__close-btn").addEventListener("click", closePopup)
 
